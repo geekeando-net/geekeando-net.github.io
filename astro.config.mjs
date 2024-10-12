bb011e12e5cbef7b17e225ac602b8a5a1662260f
Draexx <p.carranza.is@gmail.com>
2024-10-12 03:03:44 -0400

    vercel
    configuracion de analytics de vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
+  output: 'hybrid',
   integrations: [
     tailwind(),
-  ]
+  ],
+  adapter: vercel(
+    {
+      webAnalytics: {
+          enabled:true,
+      },
+    }
+  ),
 });
