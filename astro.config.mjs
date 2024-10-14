ef7bade2ab48c4a74d28d712b804f57cb15f985e
Draexx <p.carranza.is@gmail.com>
2024-10-14 07:30:19 -0400

    menu par amobile
    se corrijio la parte del menu par amobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  output: 'hybrid',
+  output: 'server',
   integrations: [
     tailwind(),
   ],
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/preline/preline.js',
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		// require('@tailwindcss/forms'),
+		require('preline/plugin'),
+	],
 }
