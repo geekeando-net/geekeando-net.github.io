8ab15e11e06e51651ebd0ecc8189bdbe9cce7f52
Draexx <p.carranza.is@gmail.com>
2024-11-07 23:49:11 -0400

    componentes
    se creo componente titlesection, y card para about

---
+++ src/utils/stringsAbout.ts
@@ -0,0 +1,30 @@
+const stringsAbout = [
+    {
+        title: 'Geekeando: Tu universo geek al alcance de un clic.',
+        text1: 'Somos una comunidad apasionada que te invita a sumergirte en el fascinante mundo geek. En Geekeando, encontrarás todo lo que necesitas para alimentar tu curiosidad y conectar con otros amantes de la cultura pop.',
+        text2: '',
+        text3: '',
+    },
+    {
+        title: 'Nuestra misión',
+        text1: '<b>Informar:</b> Te mantenemos actualizado sobre las últimas novedades del mundo geek, desde videojuegos y anime hasta ciencia ficción y tecnología.',
+        text2: '<b>Crear comunidad:</b> Fomentamos un espacio donde puedas compartir tus intereses, conocer a personas con tus mismas pasiones y participar en conversaciones enriquecedoras.',
+        text3: '<b>Inspirar:</b> Despertamos tu imaginación y te motivamos a explorar nuevos horizontes dentro del universo geek.',
+    },
+    {
+        title: '¿A quién va dirigido Geekeando?',
+        text1: 'A todos aquellos que sienten curiosidad por el mundo geek y desean ampliar sus conocimientos. Si eres un fanático de los videojuegos, el anime, los cómics, la ciencia ficción o la tecnología, este es tu lugar.',
+        text2: '',
+        text3: '',
+    },
+    {
+        title: '¿Qué nos hace únicos?',
+        text1: '<b>Contenido de calidad:</b> Ofrecemos artículos, reseñas y análisis rigurosos y entretenidos.',
+        text2: '<b>Comunidad activa:</b> Nuestra comunidad es un espacio acogedor donde todos son bienvenidos.',
+        text3: '<b>Pasión por lo geek:</b> Nuestro equipo está formado por verdaderos apasionados que comparten tu misma afición.',
+    },
+];
+
+export default {
+    stringsAbout,
+};
