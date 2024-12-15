9721af8dd1402fb97e1cb88b7484d20cbd5b2769
Draexx <p.carranza.is@gmail.com>
2024-12-15 15:40:52 -0400

    Eliminar interfaces no utilizadas y actualizar la importación de @astrojs/vercel en la configuración
    Signed-off-by: Draexx <p.carranza.is@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel';
 
 import tailwind from '@astrojs/tailwind';
 
