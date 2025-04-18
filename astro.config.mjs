0c5919a62dbe849bc37f099a83189882d9377ca8
Draexx <p.carranza.is@gmail.com>
2025-04-18 19:44:58 -0400

    Implement code changes to enhance functionality and improve performance
    Signed-off-by: Draexx <p.carranza.is@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 import icon from 'astro-icon';
 import tailwind from '@astrojs/tailwind';
 
