1d1a008be26d6e79cd316a363780c720103e8760
Draexx <p.carranza.is@gmail.com>
2025-04-18 19:02:23 -0400

    Refactor code structure for improved readability and maintainability
    Signed-off-by: Draexx <p.carranza.is@gmail.com>

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
-import { c as createExports } from './chunks/entrypoint_C6TAfJCl.mjs';
-import { manifest } from './manifest_BFJpnMYW.mjs';
+import { c as createExports } from './chunks/entrypoint_BitigE1S.mjs';
+import { manifest } from './manifest_DQKpEkAA.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -55,7 +55,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "a2fa868f-25c2-4998-89c0-c0c29519dc6a",
+    "middlewareSecret": "421bcda0-2b90-41c4-b25a-1ad4432946cc",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
